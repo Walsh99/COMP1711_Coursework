b4cbfc2c04823f21c39e1ec2a2c753c07ae93b31
Walsh99 <146854530+Walsh99@users.noreply.github.com>
2023-10-27 15:43:29 +0000

    added extra

--- StepCounter.c
+++ StepCounter.c
@@ -1,1 +1,13 @@
 #include "FitnessDataStruct.h"
+
+
+FILE *open_file(char filename[], char mode[]) 
+{
+    FILE *file = fopen(filename, mode);
+    if (file == NULL) 
+    {
+        perror("");
+        exit(1);
+    }
+    return file;
+}
